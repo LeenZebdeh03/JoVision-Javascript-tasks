99167da89565995cceedbb36b8857421f489697d
LeenZebdeh03 <leenalzebdeh@gmail.com>
2024-08-01 14:54:32 +0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const ele = document.querySelector(".ele");
-const ele2 = document.querySelector(".ele2");
-ele.addEventListener("click",writer);
-function writer()
-{
-
-    ele2.innerHTML="Hello, World";
-    console.log("Hello, World");
-}
